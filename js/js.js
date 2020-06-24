dd65807c827c1b146ae39c23c95cbee815416158
Mohamed187-ai <mohassan787898@gmail.com>
2020-06-24 17:22:46 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.timer').countTo();
+    
+
+    var mybutton = document.getElementById("myBtn");
+    mybutton.addEventListener("click", function(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+    mybutton = document.getElementById("myBtn");
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        }
+        else{
+            mybutton.style.display = "none";
+        }
+    }
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
+
